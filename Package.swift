4a16b37d0e77e2d763e9bd8a8756d413dbf6341d
Nikita Razumnyi <n.razumnyi@top.team>
2026-05-08 10:47:40 +0300

    Release 0.0.1

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "ChainKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ChainKit",
+            targets: ["ChainKit", "ChainKitSupport"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "ChainKit",
+            url: "https://github.com/tonkeeper/chainkit-swift/releases/download/0.0.1/ChainKit.xcframework.zip",
+            checksum: "6babcb2dcdcdff762892c3cbecf02b1e1ced05721a639822622e327d79e01a5b"
+        ),
+        .binaryTarget(
+            name: "WalletCore",
+            url: "https://github.com/trustwallet/wallet-core/releases/download/4.6.0/WalletCore.xcframework.zip",
+            checksum: "689935aff413004b18c7b32ee955716868ebcd38328c5159c69f0d5f5bcfddf0"
+        ),
+        .binaryTarget(
+            name: "WalletCoreSwiftProtobuf",
+            url: "https://github.com/trustwallet/wallet-core/releases/download/4.6.0/WalletCoreSwiftProtobuf.xcframework.zip",
+            checksum: "719b1ebc7ad174017e399cdd7fc60372b369d9712d646ebb8b4e264c4881d1d8"
+        ),
+        .target(
+            name: "ChainKitSupport",
+            dependencies: [
+                "WalletCore",
+                "WalletCoreSwiftProtobuf"
+            ],
+            path: "Sources/ChainKitSupport"
+        )
+    ]
+)
---
+++ Sources/ChainKitSupport/ChainKitSupport.swift
@@ -0,0 +1,2 @@
+// Link Trust Wallet Core alongside the Kotlin/Native framework.
+public enum ChainKitSupport {}
