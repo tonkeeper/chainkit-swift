f9d64c8ab8b5186f7b64c1a4a1f9a0ae7a5f3be7
Nikita Razumnyi <n.razumnyi@top.team>
2026-08-06 12:18:27 +0300

    Release 0.1.15

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChainKit",
-            url: "https://github.com/tonkeeper/chainkit-swift/releases/download/0.1.14/ChainKit.xcframework.zip",
-            checksum: "8943d64e096717d4ca169c19da1a9fbc739eae20001f1883273c567b9c723902"
+            url: "https://github.com/tonkeeper/chainkit-swift/releases/download/0.1.15/ChainKit.xcframework.zip",
+            checksum: "d7e2a97bd0f706d1a58474967ba6d5513a6c58d333b9a847128e1fce514bdae1"
         ),
         .binaryTarget(
             name: "WalletCore",
